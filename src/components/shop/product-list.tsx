ea833342ac4559d026fcec038adbbfd079f67501
Felipe Melo <felipemelogomes36@gmail.com>
2024-12-10 13:28:03 -0300

    feat: adjust value procuts list

--- src/components/shop/product-list.tsx
+++ src/components/shop/product-list.tsx
@@ -6,7 +6,7 @@ import ProductCard from "@/components/shop/product-card";
 async function getProducts() {
   try {
     const stripProducts = await stripe.products.list({
-      limit: 6,
+      limit: 10,
       expand: ["data.default_price"],
     });
 
