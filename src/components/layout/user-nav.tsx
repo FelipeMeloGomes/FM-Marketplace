e8b9298c3cd7f3805bf08c53af2ed2da732a619e
Felipe Melo <felipemelogomes36@gmail.com>
2024-12-15 22:29:35 -0300

    Replace avatar image with placeholder image

--- src/components/layout/user-nav.tsx
+++ src/components/layout/user-nav.tsx
@@ -30,7 +30,7 @@ export default function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@felipemelo" />
+            <AvatarImage src="https://picsum.photos/200" alt="@felipemelo" />
             <AvatarFallback>FM</AvatarFallback>
           </Avatar>
         </Button>
